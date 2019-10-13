5591e56faea1884fc0557f771f54e72736819ff4
Arrrrny <arny@paratoner.io>
2019-10-14 00:36:51 +0300

    fixed path reference

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.1.0
 
 * Extract Text from images, Especially helpful when you are extracting single character images.
+
+## 0.1.1
+
+* fixed path references
--- lib/tesseract_ocr.dart
+++ lib/tesseract_ocr.dart
@@ -4,8 +4,8 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
-import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 class TesseractOcr {
   static const String TESS_DATA_CONFIG = 'assets/tessdata_config.json';
