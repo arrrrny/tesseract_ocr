b42556ddbc385da9cd7430f3893d24e97fcb3e8c
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-05-14 20:20:41 +0200

    Make use of file_picker instead of assets

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,5 @@
-import 'dart:io';
-import 'dart:typed_data';
-
-import 'package:flutter/services.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:tesseract_ocr/tesseract_ocr.dart';
 
 void main() => runApp(MyApp());
@@ -18,45 +10,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _extractText = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String extractText;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      final Directory directory = await getTemporaryDirectory();
-      final String imagePath = join(
-        directory.path,
-        "tmp_1.jpg",
-      );
-      final ByteData data = await rootBundle.load('packages/tesseract_ocr/images/test.png');
-      final Uint8List bytes = data.buffer.asUint8List(
-        data.offsetInBytes,
-        data.lengthInBytes,
-      );
-      await File(imagePath).writeAsBytes(bytes);
-
-      extractText = await TesseractOcr.extractText(imagePath, language: "financial");
-    } on PlatformException {
-      extractText = 'Failed to extract text';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _extractText = extractText;
-    });
-  }
+  String _extractText = '';
 
   @override
   Widget build(BuildContext context) {
@@ -65,13 +19,27 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: const Text('Tesseract OCR'),
           ),
-          body: Column(
-            children: <Widget>[
-              Center(
-                child: Text('Detected Text: $_extractText\n'),
-              ),
-              Image.asset('images/test.png', package: 'tesseract_ocr', height: 30.0),
-            ],
+          body: Container(
+            padding: EdgeInsets.all(16),
+            child: ListView(
+              children: <Widget>[
+                Center(
+                  child: RaisedButton(
+                    child: Text('Select image'),
+                    onPressed: () async {
+                      var file =
+                          await FilePicker.getFilePath(type: FileType.image);
+                      _extractText = await TesseractOcr.extractText(file);
+                      setState(() {});
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Center(child: SelectableText(_extractText)),
+              ],
+            ),
           )),
     );
   }
